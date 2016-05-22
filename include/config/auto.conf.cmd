b5b98f11b28cb82eb19fa5032fd5a034662c9df8
Yannick Gicquel <ygicquel@gmail.com>
2016-05-22 10:47:01 +0200

    all: remove generated files previously commited
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- include/config/auto.conf.cmd
+++
@@ -1,8 +0,0 @@
-deps_config := \
-	Kconfig
-
-include/config/auto.conf: \
-	$(deps_config)
-
-
-$(deps_config): ;
--- include/generated/autoconf.h
+++
@@ -1,6 +0,0 @@
-/*
- * Automatically generated C config: don't edit
- * MYAPP Configuration
- */
-#define CONFIG_F_CPU 32000000
-#define CONFIG_CROSS_COMPILE "avr-"
