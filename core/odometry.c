335f793c4d65fabec29f3240f8fa5d854c8a8e06
Yannick Gicquel <ygicquel@gmail.com>
2016-06-20 13:39:37 +0200

    odometry: after checkpatch
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- core/odometry.c
+++ core/odometry.c
@@ -12,66 +12,44 @@
 
 static double wheels_distance; /*!< robot wheels distance [pulse] */
 
-void
-odometry_setup (double d)
+void odometry_setup(double d)
 {
-  wheels_distance = d;
-}
-
-void
-pose_reset (pose_t *p)
-{
-  if (p != NULL)
-    {
-      p->x = 0;
-      p->y = 0;
-      p->O = 0;
-    }
-}
-
-void
-pose_update (pose_t *p, double x, double y, double O)
-{
-  if (p != NULL)
-    {
-      p->x = x;
-      p->y = y;
-      p->O = O;
-    }
+	wheels_distance = d;
 }
 
 /**
  * \fn void limit_angle (void)
  * \brief limit angle from -pi to +pi
- * */
-void
-limit_angle (pose_t *p)
+ */
+static void limit_angle(pose_t *p)
 {
-  /* limit angle from -pi to +pi */
-  if (p->O > (M_PI * wheels_distance)) /* > +pi */
-    {
-      p->O -= 2.0 * (M_PI * wheels_distance);
-    }
-  if (p->O < -(M_PI * wheels_distance)) /* < -pi */
-    {
-      p->O += 2.0 * (M_PI * wheels_distance);
-    }
+	/* limit angle from -pi to +pi */
+	if (p->O > (M_PI * wheels_distance)) /* > +pi */
+		p->O -= 2.0 * (M_PI * wheels_distance);
+
+	if (p->O < -(M_PI * wheels_distance)) /* < -pi */
+		p->O += 2.0 * (M_PI * wheels_distance);
 }
 
 /**
  * \fn void odometry_by_segment (const double distance, const double angle)
- * \brief update new robot pose_t (x, y, O) approximated by straight line segments
+ * \brief update new robot pose_t (x, y, O) approximated by straight line
+ *        segments
  * \param distance : delta value for distance [pulse]
  * \param angle : delta value for angle [pulse]
- * */
-void
-odometry_by_segment (pose_t *p, const double distance, const double angle)
+ */
+static void
+odometry_by_segment(pose_t *p, const double distance, const double angle)
 {
-  double O_rad = p->O / wheels_distance; /* [radian] */
-  p->x += distance * cos (O_rad);
-  p->y += distance * sin (O_rad);
-  p->O += angle;
-  limit_angle (p);
+	double O_rad;
+
+	O_rad = p->O / wheels_distance; /* [radian] */
+
+	p->x += distance * cos(O_rad);
+	p->y += distance * sin(O_rad);
+	p->O += angle;
+
+	limit_angle(p);
 }
 
 /**
@@ -79,42 +57,39 @@ odometry_by_segment (pose_t *p, const double distance, const double angle)
  * \brief update new robot pose_t (x, y, O) approximated by an arc
  * \param distance : delta value for distance [pulse]
  * \param angle : delta value for angle [pulse]
- * */
-void
-odometry_by_arc (pose_t *p, const double distance, const double angle)
+ */
+static void
+odometry_by_arc(pose_t *p, const double distance, const double angle)
 {
-  double O_rad = p->O / wheels_distance; /* [radian] */
-  if (angle == 0)
-    {
-      /* robot pose */
-      p->x += distance * cos (O_rad);
-      p->y += distance * sin (O_rad);
-    }
-  else
-    {
-      /* radius and angle oh the arc */
-      double a = angle / wheels_distance;
-      double r = distance / a;
-      /* coordinates of the center oh the arc */
-      double xo = p->x - r * sin (O_rad);
-      double yo = p->y + r * cos (O_rad);
-      /* robot pose */
-      p->O += a;
-      p->x = xo + r * sin (O_rad);
-      p->y = yo - r * cos (O_rad);
-      limit_angle (p);
-    }
+	double O_rad;
+
+	O_rad = p->O / wheels_distance; /* [radian] */
+
+	if (angle == 0.0) {
+		/* robot pose */
+		p->x += distance * cos(O_rad);
+		p->y += distance * sin(O_rad);
+	} else {
+		/* radius and angle oh the arc */
+		double a = angle / wheels_distance;
+		double r = distance / a;
+		/* coordinates of the center oh the arc */
+		double xo = p->x - r * sin(O_rad);
+		double yo = p->y + r * cos(O_rad);
+		/* robot pose */
+		p->O += a;
+		p->x = xo + r * sin(O_rad);
+		p->y = yo - r * cos(O_rad);
+		limit_angle(p);
+	}
 }
 
 void
-odometry_update (pose_t *p, polar_t *robot_speed, const uint8_t approximation)
+odometry_update(pose_t *p, polar_t *robot_speed, const uint8_t approximation)
 {
-  if (approximation == ARC)
-    {
-      odometry_by_arc (p, robot_speed->distance, robot_speed->angle);
-    }
-  else
-    {
-      odometry_by_segment (p, robot_speed->distance, robot_speed->angle);
-    }
+	if (approximation == ARC)
+		odometry_by_arc(p, robot_speed->distance, robot_speed->angle);
+	else
+		odometry_by_segment(p,
+				    robot_speed->distance, robot_speed->angle);
 }
--- include/odometry.h
+++ include/odometry.h
@@ -16,59 +16,33 @@
 /**
  * \struct pose
  */
-typedef struct
-{
-  double x; /*!< x-position [pulse] */
-  double y; /*!< y-position [pulse] */
-  double O; /*!< 0-orientation [pulse] */
+typedef struct {
+	double x; /*!< x-position [pulse] */
+	double y; /*!< y-position [pulse] */
+	double O; /*!< 0-orientation [pulse] */
 } pose_t;
 
-typedef struct
-{
-  int16_t distance;
-  int16_t angle;
+typedef struct {
+	int16_t distance;
+	int16_t angle;
 } polar_t;
 
 /**
- * \struct polar_pose
- */
-typedef struct
-{
-  double d; /*!< distance position [pulse] */
-  double a; /*!< angle position [pulse] */
-  double O; /*!< 0-orientation [pulse] */
-} polar_pose; /* polar pose */
-
-/**
- * \fn void odometry_setup (pose_t *p, double d)
+ * \fn odometry_setup
  * \brief odometry pose_t and wheels_distance setup
  * \param p : robot pose
  * \param d : distance between wheels [pulse]
- * */
-void
-odometry_setup (double d);
-
-void
-pose_reset (pose_t *p);
+ */
+void odometry_setup(double d);
 
 /**
- * \fn void pose_update (pose_t *p, double x, double y, double O)
- * \brief update pose
- * \param p pose_t to update
- * \param x
- * \param y
- * \param O
- *  */
-void
-pose_update (pose_t *p, double x, double y, double O);
-
-/**
- * \fn void odometry_update (const double distance, const double angle, const uint8_t approximation)
+ * \fn odometry_update
  * \brief update new robot pose_t (x, y, O) by ARC or SEGMENT approximation
  * \param polar_t : delta value for distance [pulse]
  * \param approximation : SEGMENT (default) or ARC
- * */
-void
-odometry_update (pose_t *p, polar_t *robot_speed, const uint8_t approximation);
+ */
+void odometry_update(pose_t *p,
+		     polar_t *robot_speed,
+		     const uint8_t approximation);
 
 #endif /* ODOMETRY_H_ */
