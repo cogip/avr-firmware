958a46bd5aaba247074058ddeafa553e950ff225
Yannick Gicquel <ygicquel@gmail.com>
2016-06-20 13:47:21 +0200

    pid: after checkpatch
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- core/pid.c
+++ core/pid.c
@@ -7,47 +7,41 @@
 
 #include "pid.h"
 
-void
-pid_setup (PID *pid, const double kp, const double ki, const double kd)
+void pid_setup(PID *pid, const double kp, const double ki, const double kd)
 {
-  pid->kp = kp;
-  pid->ki = ki;
-  pid->kd = kd;
+	pid->kp = kp;
+	pid->ki = ki;
+	pid->kd = kd;
 }
 
-void
-pid_reset (PID *pid)
+void pid_reset(PID *pid)
 {
-  pid->previous_error = 0;
-  pid->ti = 0;
+	pid->previous_error = 0;
+	pid->ti = 0;
 }
 
-double
-pid_controller (const double error, PID *pid)
+double pid_controller(const double error, PID *pid)
 {
-  double p, i, d;
+	double p, i, d;
 
-  /* proportional */
-  p = error * pid->kp;
+	/* proportional */
+	p = error * pid->kp;
 
-  /* integral */
-  pid->ti += error;      // error sum
+	/* integral */
+	pid->ti += error;	/* error sum */
 
-  /* integral limitation */
-  if (pid->ti > INTEGRAL_LIMIT)
-    {
-      pid->ti = INTEGRAL_LIMIT;
-    }
-  else if (pid->ti < -INTEGRAL_LIMIT)
-    {
-      pid->ti = -INTEGRAL_LIMIT;
-    }
-  i = pid->ti * pid->ki;
+	/* integral limitation */
+	if (pid->ti > INTEGRAL_LIMIT)
+		pid->ti = INTEGRAL_LIMIT;
+	else if (pid->ti < -INTEGRAL_LIMIT)
+		pid->ti = -INTEGRAL_LIMIT;
 
-  /* derivative */
-  d = error - pid->previous_error;
-  d *= pid->kd;
-  pid->previous_error = error;      // backup the previous error
+	i = pid->ti * pid->ki;
 
-  return p + i + d;
+	/* derivative */
+	d = error - pid->previous_error;
+	d *= pid->kd;
+	pid->previous_error = error;	/* backup the previous error */
+
+	return p + i + d;
 }
--- include/pid.h
+++ include/pid.h
@@ -8,47 +8,43 @@
 #ifndef PID_H_
 #define PID_H_
 
-#define INTEGRAL_LIMIT	500000
+#define INTEGRAL_LIMIT		500000
 
 /**
  * \struct PID
  */
-typedef struct
-{
-  double kp; /*!< proportional gain */
-  double ki; /*!< integral gain */
-  double kd; /*!< derivative gain */
-  double ti; /*!< error sum */
-  double previous_error; /*!< previous sum */
+typedef struct {
+	double kp; /*!< proportional gain */
+	double ki; /*!< integral gain */
+	double kd; /*!< derivative gain */
+	double ti; /*!< error sum */
+	double previous_error; /*!< previous sum */
 } PID;
 
 /**
- * \fn void pid_setup(PID *pid, const double kp, const double ki, const double kd)
+ * \fn pid_setup
  * \brief PID setup
  * \param pid struct PID
  * \param kp proportional gain
  * \param ki integral gain
  * \param kd derivative gain
- * */
-void
-pid_setup (PID *pid, const double kp, const double ki, const double kd);
+ */
+void pid_setup(PID *pid, const double kp, const double ki, const double kd);
 
 /**
- * \fn void pid_reset(PID *pid)
+ * \fn pid_reset
  * \brief PID reset pid parameters
  * \param pid struct PID
- * */
-void
-pid_reset (PID *pid);
+ */
+void pid_reset(PID *pid);
 
 /**
- * \fn double pid_controller(const double error, PID *pid)
+ * \fn pid_controller
  * \brief compute pid controller
  * \param error
  * \param pid
  * \return the variable that will be adjusted by the pid
- * */
-double
-pid_controller (const double error, PID *pid);
+ */
+double pid_controller(const double error, PID *pid);
 
 #endif /* PID_H_ */
