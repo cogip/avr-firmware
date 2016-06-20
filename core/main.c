e9255b5fb3f21afd638e7e3c28f1c86080e7dc31
Yannick Gicquel <ygicquel@gmail.com>
2016-06-20 18:15:35 +0200

    main: after checkpatch
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- core/main.c
+++ core/main.c
@@ -7,37 +7,37 @@
 
 /**
  * PORTA : ANA input
- * 	PA0 - PA7 : IR
+ *	PA0 - PA7 : IR
  * PORTB : JTAG + ANA inputs
  * PORTC : Communication
- * 	PC0 (SDA) : i2c communication
- * 	PC1 (SCL) : i2c communication
- * 	PC2 (RX) : Debug communication
- * 	PC3 (TX) :
- * 	PC6 (RX1) : (Lidar communication)
- * 	PC7 (TX1) :
+ *	PC0 (SDA) : i2c communication
+ *	PC1 (SCL) : i2c communication
+ *	PC2 (RX) : Debug communication
+ *	PC3 (TX) :
+ *	PC6 (RX1) : (Lidar communication)
+ *	PC7 (TX1) :
  * PORTD : Communication
- * 	PD2 (RX2) : (MotherBoard communication)
- * 	PD3 (TX2) :
- * 	PD4 : DIR1 left motor
- * 	PD5 : DIR2 right motor
- * 	PD6 : DIR3 elevator motor
+ *	PD2 (RX2) : (MotherBoard communication)
+ *	PD3 (TX2) :
+ *	PD4 : DIR1 left motor
+ *	PD5 : DIR2 right motor
+ *	PD6 : DIR3 elevator motor
  * PORTE : Timer + PWM
- * 	PE0 (OC0A) : PWM1 left motor
- * 	PE1 (OC0B) : PWM2 right motor
- * 	PE2 (OC0C) : PWM3
- * 	PE3 (OC0D) : -
- * 	PE4 : encoder A1 left wheel
- * 	PE5 : encoder B1
+ *	PE0 (OC0A) : PWM1 left motor
+ *	PE1 (OC0B) : PWM2 right motor
+ *	PE2 (OC0C) : PWM3
+ *	PE3 (OC0D) : -
+ *	PE4 : encoder A1 left wheel
+ *	PE5 : encoder B1
  * PORTF : Timer decoder quadrature
- * 	PF0 : encoder A2 right wheel
- * 	PF1 : encoder B2
- * 	PF4 : encoder A3
- * 	PF5 : encoder B3
+ *	PF0 : encoder A2 right wheel
+ *	PF1 : encoder B2
+ *	PF4 : encoder A3
+ *	PF5 : encoder B3
  * PORTH : Digital port
- * 	PH0 : capteur TOR 19
- * 	PH1 : 18
- * 	PH2 : 17
+ *	PH0 : capteur TOR 19
+ *	PH1 : 18
+ *	PH2 : 17
  * PORTJ :
  * PORTK :
  * PORTQ : TOSC
@@ -65,289 +65,271 @@
 #include "odometry.h"
 #include "route.h"
 
-/**
- * \fn void clock_setup (void)
- * \brief setup system clock 32 MHz
- *
- */
-void
-clock_setup (void)
+volatile uint8_t controller_flag;
+
+polar_t	robot_speed;
+polar_t	speed_setpoint		= { 60, 60 };
+polar_t	motor_command;
+pose_t	robot_pose		= { 1856.75, 0, 0 }; /* position absolue */
+pose_t	pose_setpoint		= { 0, 0, 0 };
+uint8_t	pose_reached;
+
+int16_t	speed_elevator;
+int16_t	distance_elevator;
+int16_t	tempo;
+
+static void clock_setup(void)
 {
-  /* Configuration change protection : Protected IO register
-   * disable automatically all interrupts for the next four CPU instruction cycles */
-  CCP = CCP_IOREG_gc;
+	/* Configuration change protection : Protected IO register
+	 * disable automatically all interrupts for the next
+	 * four CPU instruction cycles
+	 */
+	CCP = CCP_IOREG_gc;
 
-  /* Oscillator : 32MHz Internal Oscillator Enable */
-  OSC.CTRL = OSC_RC32MEN_bm;
+	/* Oscillator : 32MHz Internal Oscillator Enable */
+	OSC.CTRL = OSC_RC32MEN_bm;
 
-  /* Wait for the internal 32 MHz RC oscillator to stabilize */
-  while (!(OSC.STATUS & OSC_RC32MRDY_bm))
-    ;
+	/* Wait for the internal 32 MHz RC oscillator to stabilize */
+	while (!(OSC.STATUS & OSC_RC32MRDY_bm))
+		;
 
-  /* Configuration change protection : Protected IO register */
-  CCP = CCP_IOREG_gc;
+	/* Configuration change protection : Protected IO register */
+	CCP = CCP_IOREG_gc;
 
-  /* Clock : 32MHz Internal Oscillator */
-  CLK.CTRL = CLK_SCLKSEL_RC32M_gc; //CLK.CTRL = CLK_SCLKSEL_RC2M_gc;
+	/* Clock : 32MHz Internal Oscillator */
+	CLK.CTRL = CLK_SCLKSEL_RC32M_gc;
 }
 
-/**
- * \fn void interrupt_setup (void)
- */
-void
-interrupt_setup (void)
+static void interrupt_setup(void)
 {
-  /* Programmable Multilevel Interrupt Controller */
-  PMIC.CTRL |= PMIC_LOLVLEN_bm; /* Low-level Interrupt Enable */
+	/* Programmable Multilevel Interrupt Controller */
+	PMIC.CTRL |= PMIC_LOLVLEN_bm; /* Low-level Interrupt Enable */
 
-  /* global interrupt enable */
-  sei ();
+	/* global interrupt enable */
+	sei();
 }
 
-/**
- * \fn void pin_setup (void)
- */
-void
-pin_setup (void)
+static void pin_setup(void)
 {
-  /* analog to digital conversion */
-  PORTA.DIR = 0x00; /*!< PORTA as input pin */
-  PORTA.OUT = 0x00;
-  PORTA.PIN0CTRL = PORT_ISC_INPUT_DISABLE_gc;
+	/* analog to digital conversion */
+	PORTA.DIR = 0x00; /*!< PORTA as input pin */
+	PORTA.OUT = 0x00;
+	PORTA.PIN0CTRL = PORT_ISC_INPUT_DISABLE_gc;
 
-  /* twi configuration pin */
-  PORTC.DIRSET = PIN1_bm; /*!< PC1 (SCL) as output pin */
-  /* usart configuration pin */
-  PORTC.DIRCLR = PIN2_bm; /*!< PC2 (RDX0) as input pin */
-  PORTC.DIRSET = PIN3_bm; /*!< PC3 (TXD0) as output pin */
+	/* twi configuration pin */
+	PORTC.DIRSET = PIN1_bm; /*!< PC1 (SCL) as output pin */
+	/* usart configuration pin */
+	PORTC.DIRCLR = PIN2_bm; /*!< PC2 (RDX0) as input pin */
+	PORTC.DIRSET = PIN3_bm; /*!< PC3 (TXD0) as output pin */
 
-  /* Configure PD4, PD5 and PD6 as output pin */
-  PORTD.DIR = 0xFF;
+	/* Configure PD4, PD5 and PD6 as output pin */
+	PORTD.DIR = 0xFF;
 
-  /* Configure PE0, PE1 and PE2 as output pin */
-  PORTE.DIRSET = PIN0_bm;
-  PORTE.DIRSET = PIN1_bm;
-  PORTE.DIRSET = PIN2_bm;
-  /* Configure PE4 and PE5 as input pin */
-  PORTE.DIRCLR = PIN4_bm;
-  PORTE.DIRCLR = PIN5_bm;
-  PORTE.PIN4CTRL = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc; // A : QDPH0 - D0
-  PORTE.PIN5CTRL = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc; // B : QDPH90 - D1
+	/* Configure PE0, PE1 and PE2 as output pin */
+	PORTE.DIRSET = PIN0_bm;
+	PORTE.DIRSET = PIN1_bm;
+	PORTE.DIRSET = PIN2_bm;
+	/* Configure PE4 and PE5 as input pin */
+	PORTE.DIRCLR = PIN4_bm;
+	PORTE.DIRCLR = PIN5_bm;
+	/* A : QDPH0 - D0 */
+	PORTE.PIN4CTRL = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc;
+	/* B : QDPH90 - D1 */
+	PORTE.PIN5CTRL = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc;
 
-  /* PORTF encoder counter (PF0, PF1, PF4 and PF5) as input pin, Set QDPH0 and QDPH1 sensing level
-   * index not used here
-   */
-  PORTF.DIRCLR = PIN0_bm;
-  PORTF.DIRCLR = PIN1_bm;
-  PORTF.PIN0CTRL = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc; // A : QDPH0 - D0
-  PORTF.PIN1CTRL = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc; // B : QDPH90 - D1
-  PORTF.DIRCLR = PIN4_bm;
-  PORTF.DIRCLR = PIN5_bm;
-  PORTF.PIN4CTRL = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc; // A : QDPH0 - D0
-  PORTF.PIN5CTRL = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc; // B : QDPH90 - D1
+	/* PORTF encoder counter (PF0, PF1, PF4 and PF5) as input pin,
+	 * Set QDPH0 and QDPH1 sensing level index not used here
+	 */
+	PORTF.DIRCLR = PIN0_bm;
+	PORTF.DIRCLR = PIN1_bm;
+	/* A : QDPH0 - D0 */
+	PORTF.PIN0CTRL = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc;
+	/* B : QDPH90 - D1 */
+	PORTF.PIN1CTRL = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc;
+	PORTF.DIRCLR = PIN4_bm;
+	PORTF.DIRCLR = PIN5_bm;
+	/* A : QDPH0 - D0 */
+	PORTF.PIN4CTRL = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc;
+	/* B : QDPH90 - D1 */
+	PORTF.PIN5CTRL = PORT_ISC_LEVEL_gc | PORT_OPC_PULLUP_gc;
 
-  /**
-   * PORTH and PORTJ as digital input
-   */
-  PORTH.DIR = 0x00;
-  PORTJ.DIRCLR = PIN0_bm;
-  PORTJ.DIRCLR = PIN1_bm;
+	/**
+	 * PORTH and PORTJ as digital input
+	 */
+	PORTH.DIR = 0x00;
+	PORTJ.DIRCLR = PIN0_bm;
+	PORTJ.DIRCLR = PIN1_bm;
 }
 
 /*
  * setup quadrature decoder A & B (index is not used here)
  * use CH0, CH2 and CH4
  */
-void
-qdec_setup (void)
+void qdec_setup(void)
 {
-  /* Configure event channel x assign to pin x */
-  EVSYS.CH0MUX = EVSYS_CHMUX_PORTE_PIN4_gc; // A & B inputs to quadrature decoder
-  //EVSYS.CH0MUX = EVSYS_CHMUX_PORTE_PIN6_gc; // index input to quadrature decoder
-  EVSYS.CH0CTRL = EVSYS_QDEN_bm
-      | EVSYS_DIGFILT_2SAMPLES_gc /*| EVSYS_QDIEN_bm*/;
-  xmega_timer_1_qdec_mode_setup (&TCE1, TC_EVSEL_CH0_gc, 500);
+	/* Configure event channel x assign to pin x */
+	/* A & B inputs to quad-decoder */
+	EVSYS.CH0MUX = EVSYS_CHMUX_PORTE_PIN4_gc;
+	EVSYS.CH0CTRL = EVSYS_QDEN_bm
+			| EVSYS_DIGFILT_2SAMPLES_gc /*| EVSYS_QDIEN_bm*/;
+	xmega_timer_1_qdec_mode_setup(&TCE1, TC_EVSEL_CH0_gc, 500);
 
-  /* Configure event channel x assign to pin x */
-  EVSYS.CH2MUX = EVSYS_CHMUX_PORTF_PIN0_gc; // A & B inputs to quadrature decoder
-  //EVSYS.CH2MUX = EVSYS_CHMUX_PORTF_PIN2_gc; // index input to quadrature decoder
-  EVSYS.CH2CTRL = EVSYS_QDEN_bm
-      | EVSYS_DIGFILT_2SAMPLES_gc /*| EVSYS_QDIEN_bm*/;
-  //EVSYS.CH1CTRL = EVSYS_DIGFILT_2SAMPLES_gc;
-  xmega_timer_0_qdec_mode_setup (&TCF0, TC_EVSEL_CH2_gc, 500);
+	/* Configure event channel x assign to pin x */
+	/* A & B inputs to quad-decoder */
+	EVSYS.CH2MUX = EVSYS_CHMUX_PORTF_PIN0_gc;
+	EVSYS.CH2CTRL = EVSYS_QDEN_bm
+			| EVSYS_DIGFILT_2SAMPLES_gc /*| EVSYS_QDIEN_bm*/;
 
-  /* Configure event channel x assign to pin x */
-  /*EVSYS.CH4MUX = EVSYS_CHMUX_PORTF_PIN4_gc; // A & B inputs to quadrature decoder
-   EVSYS.CH4MUX = EVSYS_CHMUX_PORTF_PIN6_gc; // index input to quadrature decoder
-   EVSYS.CH4CTRL = EVSYS_QDEN_bm
-   | EVSYS_DIGFILT_2SAMPLES_gc;
-   EVSYS.CH1CTRL = EVSYS_DIGFILT_2SAMPLES_gc;
-   xmega_timer_1_qdec_mode_setup (&TCF1, TC_EVSEL_CH4_gc, 512);*/
+	xmega_timer_0_qdec_mode_setup(&TCF0, TC_EVSEL_CH2_gc, 500);
 }
 
-volatile uint8_t controller_flag = 0;
-
-ISR (TCC0_OVF_vect)
+/* Timer 0 Overflow interrupt */
+ISR(TCC0_OVF_vect)
 {
-  controller_flag = 1;
+	controller_flag = 1;
 }
 
 /**
  *
  */
-void
-setup (void)
+static void setup(void)
 {
-  clock_setup ();
-  pin_setup ();
-  interrupt_setup ();
+	clock_setup();
+	pin_setup();
+	interrupt_setup();
 
-  /* timer setup */
-  xmega_timer_0_normal_mode_setup (&TCC0, 625, TC_CLKSEL_DIV1024_gc);
+	/* timer setup */
+	controller_flag = 0;
+	xmega_timer_0_normal_mode_setup(&TCC0, 625, TC_CLKSEL_DIV1024_gc);
 
-  /* setup usart communication */
-  xmega_usart_setup (&USARTC0);
+	/* setup usart communication */
+	xmega_usart_setup(&USARTC0);
 
-  /* setup TWI communication with SD21 */
-  sd21_setup (&TWIC);
-  action_setup (); //TODO commenter pour debug
+	/* setup TWI communication with SD21 */
+	sd21_setup(&TWIC);
 
-  /* setup frequency waveform generation (PWM) */
-  xmega_timer_0_pwm_mode_setup (&TCE0, 200, TC_CLKSEL_DIV8_gc); /* 200 pour 20KHz -100 pour pour 40 KHz */
-  xmega_timer_0_pwm_enable (&TCE0, 0); // PE0
-  xmega_timer_0_pwm_enable (&TCE0, 1); // PE1
-  xmega_timer_0_pwm_enable (&TCE0, 2); // PE2
+	action_setup(); /* TODO: commenter pour debug */
 
-  /* setup qdec */
-  qdec_setup ();
+	/* setup frequency waveform generation (PWM) */
+	/* note: 200 pour 20KHz -100 pour pour 40 KHz */
+	xmega_timer_0_pwm_mode_setup(&TCE0, 200, TC_CLKSEL_DIV8_gc);
+	xmega_timer_0_pwm_enable(&TCE0, 0); /* PE0 */
+	xmega_timer_0_pwm_enable(&TCE0, 1); /* PE1 */
+	xmega_timer_0_pwm_enable(&TCE0, 2); /* PE2 */
 
-  /* setup analog conversion */
-  xmega_adc_setup (&ADCA);
+	/* setup qdec */
+	qdec_setup();
 
-  /* controller setup */
-  odometry_setup (WHEELS_DISTANCE);
-  controller_setup ();
+	/* setup analog conversion */
+	xmega_adc_setup(&ADCA);
+
+	/* controller setup */
+	odometry_setup(WHEELS_DISTANCE);
+	controller_setup();
 }
 
-polar_t robot_speed;
-polar_t speed_setpoint =
-  { 60, 60 };
-polar_t motor_command;
-pose_t robot_pose =
-  { 1856.75, 0, 0 }; /* position absolue */
-pose_t pose_setpoint =
-  { 0, 0, 0 };
-uint8_t pose_reached;
-
-int16_t speed_elevator;
-int16_t distance_elevator;
-int16_t tempo = 0;
 /**
  * TODO 90 s
  * TODO gestion bras maintien position plus fermé
  * TODO init trappe pop corn
  */
-int
-main (void)
+int main(void)
 {
-  uint8_t all_irs[] =
-    { 2, 3, 4, 5, 6, 7 };
-  setup ();
-  xmega_adc_read (&ADCA, 0); // start first conversion
-  xmega_usart_transmit (&USARTC0, 0xAA);
+	uint8_t all_irs[] = { 2, 3, 4, 5, 6, 7 };
+	uint8_t stop = 0;
 
-  while (detect_start ())
-    {
-      gestion_tour ();
-    }
+	setup();
+	/* start first conversion */
+	xmega_adc_read(&ADCA, 0);
+	xmega_usart_transmit(&USARTC0, 0xAA);
 
-  /* main loop */
-  while (tempo < 4500)
-    {
-      if (controller_flag == 1)
-	{
-	  /* we enter here every 20ms */
-	  tempo++;
+	while (detect_start())
+		gestion_tour();
 
-          /* catch speed */
-	  robot_speed = read_encoder ();
+	/* main loop */
+	while (tempo < 4500) {
+		if (controller_flag == 1) {
+			/* we enter here every 20ms */
+			tempo++;
 
-          /* convert to position */
-	  odometry_update (&robot_pose, &robot_speed, SEGMENT);
+			/* catch speed */
+			robot_speed = read_encoder();
 
-          /* get next pose_t to reach */
-	  pose_setpoint = route_update ();
+			/* convert to position */
+			odometry_update(&robot_pose, &robot_speed, SEGMENT);
 
-	  pose_setpoint.x *= PULSE_PER_MM;
-	  pose_setpoint.y *= PULSE_PER_MM;
-	  pose_setpoint.O *= PULSE_PER_DEGREE;
+			/* get next pose_t to reach */
+			pose_setpoint = route_update();
 
-          /* mirror mode: invert path regarding bot's camp */
-	  if (detect_color ())
-	    {
-	      pose_setpoint.y *= -1;
-	      pose_setpoint.O *= -1;
-	    }
+			pose_setpoint.x *= PULSE_PER_MM;
+			pose_setpoint.y *= PULSE_PER_MM;
+			pose_setpoint.O *= PULSE_PER_DEGREE;
 
-          /* collision detect */
+			/* mirror mode: invert path regarding bot's camp */
+			if (detect_color()) {
+				pose_setpoint.y *= -1;
+				pose_setpoint.O *= -1;
+			}
 
-	  /*uint8_t side_irs[] =
-	   { 0, 1 };
-	   uint8_t rear_irs[] =
-	   { 2, 3 };
-	   uint8_t front_irs[] =
-	   { 4, 5, 6, 7 };*/
+			/* collision detect */
 
-	  uint8_t stop = 0;
+#if 0
+			uint8_t side_irs[] = { 0, 1 };
+			uint8_t rear_irs[] = { 2, 3 };
+			uint8_t front_irs[] = { 4, 5, 6, 7 };
 
-	  /*if ((right_command > 0) && (left_command > 0))
-	   stop = stop_robot (front_irs, 4);
-	   else if ((right_command < 0) && (left_command < 0))
-	   stop = stop_robot (rear_irs, 2);
-	   else*/
-	  stop = stop_robot (all_irs, 6);
+			if ((right_command > 0) && (left_command > 0))
+			stop = stop_robot(front_irs, 4);
+			else if ((right_command < 0) && (left_command < 0))
+			stop = stop_robot(rear_irs, 2);
+			else
+#endif
+			stop = stop_robot(all_irs, 6);
 
-	  if (stop && get_route_index ())
-	    {
-	      speed_setpoint.distance = 0;
-	      speed_setpoint.angle = 0;
-	      /*if ((stop > 1) && (get_can_retro ()))
-	       down_route_index ();*/
-	    }
-	  else
-	    {
-	      speed_setpoint.distance = 60;
-	      speed_setpoint.angle = 60;
-	    }
+			if (stop && get_route_index()) {
+				speed_setpoint.distance = 0;
+				speed_setpoint.angle = 0;
+#if 0
+				if ((stop > 1) && (get_can_retro()))
+				down_route_index();
+#endif
+			} else {
+				speed_setpoint.distance = 60;
+				speed_setpoint.angle = 60;
+			}
 
-          /* PID / feedback control */
-	  //motor_command = speed_controller (speed_setpoint, robot_speed);
-	  motor_command = controller_update (pose_setpoint, robot_pose,
-					     speed_setpoint, robot_speed);
+			/* PID / feedback control */
+#if 0
+			motor_command = speed_controller(speed_setpoint,
+							 robot_speed);
+#endif
+			motor_command = controller_update(pose_setpoint,
+							  robot_pose,
+							  speed_setpoint,
+							  robot_speed);
 
-          /* set speed to wheels */
-	  motor_drive (motor_command);
+			/* set speed to wheels */
+			motor_drive(motor_command);
 
-	  controller_flag = 0;
+			controller_flag = 0;
+		}
+
+		attraper_cup();
+		read_analog_sensor();
+		gestion_tour();
 	}
 
-      attraper_cup ();
-      read_analog_sensor ();
-      gestion_tour ();
-    }
+	/* final position */
+	while (1) {
+		open_pince();
+		open_door();
+		set_release_right_cup();
+		set_release_left_cup();
+		motor_command.distance = 0;
+		motor_command.angle = 0;
+		motor_drive(motor_command);
+	}
 
-  /* final position */
-  while (1)
-    {
-      open_pince ();
-      open_door ();
-      set_release_right_cup ();
-      set_release_left_cup ();
-      motor_command.distance = 0;
-      motor_command.angle = 0;
-      motor_drive (motor_command);
-    }
-
-  /* we never reach this point */
-  return 0;
+	/* we never reach this point */
+	return 0;
 }
