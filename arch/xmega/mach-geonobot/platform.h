1e24490fa3bd5ee307eed79b248ef54b86bebe03
Yannick Gicquel <ygicquel@gmail.com>
2016-06-27 21:04:41 +0200

    mach-geonobot: initiate platform
    move specific platform code in machine specific directory.
    this commit contains the engines configuration.
    
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

---
+++ arch/xmega/mach-geonobot/platform.h
@@ -0,0 +1,12 @@
+#ifndef PLATFORM_H_
+#define PLATFORM_H_
+
+#include "hbridge.h"
+
+
+#define HBRIDGE_MOTOR_LEFT		0
+#define HBRIDGE_MOTOR_RIGHT		1
+
+extern hbridge_t hbridges;
+
+#endif /* PLATFORM_H_ */
--- core/motor.c
+++ core/motor.c
@@ -8,38 +8,7 @@
 
 #include "hbridge.h"
 #include "motor.h"
-
-#define HBRIDGE_MOTOR_LEFT		0
-#define HBRIDGE_MOTOR_RIGHT		1
-
-/* TCE0 ClkIn == ClkPer / 8 == 4000 KHz */
-/* Counter set to 200 for 20KHz output */
-#define TCE0_MOTOR_PRESCALER		TC_CLKSEL_DIV8_gc
-#define TCE0_MOTOR_PER_VALUE		200
-
-hbridge_t hbridges = {
-	.tc0 = &TCE0,
-	.period = TCE0_MOTOR_PER_VALUE,
-	.prescaler = TCE0_MOTOR_PRESCALER,
-
-	.pwm_port = &PORTE, /* TODO: can be 'guessed' from timer ref above */
-
-	.engine_nb = 2,
-	.engines = {
-		[HBRIDGE_MOTOR_LEFT] = {
-			/* left motor */
-			.direction_pin_port = &PORTD,
-			.direction_pin_id = PIN4_bm,
-			.pwm_channel = PIN0_bp,
-		},
-		[HBRIDGE_MOTOR_RIGHT] = {
-			/* right motor */
-			.direction_pin_port = &PORTD,
-			.direction_pin_id = PIN5_bm,
-			.pwm_channel = PIN1_bp,
-		},
-	},
-};
+#include "platform.h"
 
 void motor_drive(polar_t command)
 {
