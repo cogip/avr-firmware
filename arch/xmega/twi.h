d33a75c4bf38a39893036daf635909efc729f61e
Yannick Gicquel <ygicquel@gmail.com>
2016-07-02 19:48:36 +0200

    twi: after checkpatch
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- arch/xmega/twi.h
+++ arch/xmega/twi.h
@@ -10,15 +10,12 @@
 
 #include <avr/interrupt.h>
 
-void
-xmega_twi_master_setup (TWI_t *twi, uint16_t freq);
+void xmega_twi_master_setup(TWI_t *twi, uint16_t freq);
 
-void
-xmega_twi_master_write_handler (TWI_t *twi);
+void xmega_twi_master_write_handler(TWI_t *twi);
 
-void
-xmega_twi_write (TWI_t *twi, uint8_t slave_address, uint8_t *writeData,
-		 uint8_t bytesToWrite);
+void xmega_twi_write(TWI_t *twi, uint8_t slave_address, uint8_t *writeData,
+		     uint8_t bytesToWrite);
 
 /**
  * \fn
@@ -29,8 +26,7 @@ xmega_twi_write (TWI_t *twi, uint8_t slave_address, uint8_t *writeData,
  * \param read_data
  * \param nb_byte_to_read
  */
-void
-xmega_twi_read (TWI_t *twi, uint8_t slave_address, uint8_t *write_data,
-		uint8_t *read_data, uint8_t nb_byte_to_read);
+void xmega_twi_read(TWI_t *twi, uint8_t slave_address, uint8_t *write_data,
+		    uint8_t *read_data, uint8_t nb_byte_to_read);
 
 #endif /* XMEGA_TWI_H_ */
