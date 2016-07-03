4b5c44b53618def4ba2db9c0ccbc95dd223f7f2a
Yannick Gicquel <ygicquel@gmail.com>
2016-07-03 19:32:03 +0200

    arch/xmega: introduce gpio_t abstraction
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

---
+++ arch/xmega/gpio.c
@@ -0,0 +1,17 @@
+#include "gpio.h"
+
+inline void gpio_set_direction(gpio_port_t *p, uint8_t pin_id, uint8_t output)
+{
+	if (output)
+		p->DIRSET = (1 << pin_id);
+	else
+		p->DIRCLR = (1 << pin_id);
+}
+
+inline void gpio_set_output(gpio_port_t *p, uint8_t pin_id, int8_t value)
+{
+	if (value)
+		p->OUTSET = pin_id;
+	else
+		p->OUTCLR = pin_id;
+}
---
+++ arch/xmega/gpio.h
@@ -0,0 +1,14 @@
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include <avr/io.h>
+
+#define GPIO_DIR_IN		0
+#define GPIO_DIR_OUT		1
+
+typedef volatile PORT_t gpio_port_t;
+
+inline void gpio_set_direction(gpio_port_t *p, uint8_t pin_id, uint8_t output);
+inline void gpio_set_output(gpio_port_t *p, uint8_t pin_id, int8_t value);
+
+#endif /* GPIO_H_ */
