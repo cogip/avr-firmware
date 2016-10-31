bd3b2579f6349bfc36319022513ccadafacfbf4e
Yannick Gicquel <ygicquel@gmail.com>
2016-10-31 09:08:43 +0100

    console: add printf_ratelimited
    This is basic implementation with a constant filtering rate
    initially set to 25. This can be enhanced later to match the
    scheduler values or thread periodicity.
    
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- arch/xmega/console.h
+++ arch/xmega/console.h
@@ -4,6 +4,16 @@
 typedef void (*putchar_cb_t)(const char);
 typedef int (*getchar_cb_t)(void);
 
+#define printf_ratelimited(...) \
+do { \
+	static uint8_t _cpt = (25); \
+\
+	if (! --_cpt) { \
+		_cpt = (25); \
+		printf(__VA_ARGS__); \
+	} \
+} while(0)
+
 void console_init(putchar_cb_t, getchar_cb_t);
 
 #endif /* CONSOLE_H */
