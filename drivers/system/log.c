53a8025bebfd7b62f4f7ab4e34c3bea2b8d303bf
Yannick Gicquel <ygicquel@gmail.com>
2017-01-14 18:36:25 +0100

    log: basic numeral data buffer storage
    To catch real-time datas in Ram before extracting them through
    serial link. Due to low available memory, logs may contains
    200 values for a given series (few seconds max.)
    
    Futur possible enhancements:
     - implement a round-robin index when buffer is full, instead of stop.
     - make this feature optional and stubbed if disabled through Kconfig.
    
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- drivers/system/log.c
+++ drivers/system/log.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "log.h"
 
@@ -74,4 +75,135 @@ void print_log(int level, const char *function, const char *format, ...)
 	va_start (args, format);
 	print_log_v(level, function, format, args);
 	va_end (args);
+}
+
+void log_vect_init(datalog_t *d, uint16_t len, ...)
+{
+	va_list args;
+
+	datalog_col_t t;
+
+	d->line_cur = 0;
+	d->line_max = len;
+
+	d->col_nb = 0;
+
+	va_start (args, len);
+
+	for (t = va_arg(args, int);
+	     t != COL_END;
+	     d->col_nb++, t = va_arg(args, int))
+	{
+		uint16_t size;
+
+		/* 1st var arg is column type */
+		d->columns[d->col_nb].type = t;
+
+		switch (t) {
+		case COL_INT16:
+			size = sizeof(int16_t) * len;
+			break;
+		case COL_DOUBLE:
+			size = sizeof(double) * len;
+			break;
+		default:
+		case COL_END:
+			return;
+		}
+
+		/* allocate a vector regarding column type element size */
+		d->datas[d->col_nb] = malloc(size);
+		if (!d->datas[d->col_nb])
+			goto alloc_err;
+		memset(d->datas[d->col_nb], 0, size);
+
+		/* 2nd var arg is the column name */
+		d->columns[d->col_nb].name = va_arg(args, const char *);
+	}
+
+	va_end (args);
+
+	return;
+
+alloc_err:
+	printf("Malloc failed!\n");
+#if defined(__AVR__)
+	printf("%p, %p, %d\n",
+		__malloc_heap_end,
+		__malloc_heap_start,
+		__malloc_margin);
+#endif
+
+	for(;;);
+}
+
+inline void log_vect_reset(datalog_t *d)
+{
+	d->line_cur = 0;
+}
+
+void log_vect_setvalue(datalog_t *d, uint8_t idx, void * value)
+{
+	int16_t *i_datas;
+	double  *d_datas;
+
+	if (idx >= d->col_nb)
+		return;
+	if (d->line_cur >= d->line_max)
+		return;
+
+	switch (d->columns[idx].type) {
+	case COL_INT16:
+		i_datas = (int16_t *)d->datas[idx];
+		i_datas[d->line_cur] = *((int16_t *)value);
+		break;
+	case COL_DOUBLE:
+		d_datas = (double *)d->datas[idx];
+		d_datas[d->line_cur] = *((double *)value);
+		break;
+	case COL_END:
+		break;
+	}
+}
+
+void log_vect_storeline(datalog_t *d)
+{
+	if (d->line_cur >= d->line_max)
+		return;
+	d->line_cur += 1;
+//	d->line_cur %= d->line_max;
+}
+
+void log_vect_dumpall(datalog_t *d)
+{
+	uint8_t c;
+	uint16_t l;
+
+	int16_t *i_datas;
+	double  *d_datas;
+
+	printf("\ntime,");
+	for (c = 0; c < d->col_nb; c++) {
+		printf("%s,", d->columns[c].name);
+	}
+	printf("\n");
+
+	for (l = 0; l < d->line_cur; l++) {
+		printf("%d,", l);
+		for (c = 0; c < d->col_nb; c++) {
+			switch(d->columns[c].type) {
+			case COL_INT16:
+				i_datas = (int16_t *)d->datas[c];
+				printf("%d,", i_datas[l]);
+				break;
+			case COL_DOUBLE:
+				d_datas = (double *)d->datas[c];
+				printf("%+.2f,", d_datas[l]);
+				break;
+			case COL_END:
+				break;
+			}
+		}
+		printf("\n");
+	}
 }
--- drivers/system/log.h
+++ drivers/system/log.h
@@ -1,6 +1,8 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include <stdint.h>
+
 #define LOG_LEVEL_ERROR    0
 #define LOG_LEVEL_WARNING  1
 #define LOG_LEVEL_INFO     2
@@ -17,5 +19,49 @@ void print_log(int level, const char *function, const char *format, ...);
 #define print_info(...) _print_log(LOG_LEVEL_INFO, __VA_ARGS__)
 #define print_warn(...) _print_log(LOG_LEVEL_WARNING, __VA_ARGS__)
 #define print_err(...) _print_log(LOG_LEVEL_ERROR, __VA_ARGS__)
+
+typedef enum {
+	COL_INT16,
+	COL_DOUBLE,
+	COL_END = -1,
+} datalog_col_t;
+
+#define COL_MAX 10
+
+typedef struct {
+	uint16_t line_cur;	/* current line index to write data to */
+	uint16_t line_max;	/* number of line (i.e. vectors size) */
+
+	uint8_t col_nb;
+	struct {
+		datalog_col_t	type;
+		const char *	name;
+	} columns[COL_MAX];
+
+	void * datas[COL_MAX];
+} datalog_t;
+
+/*
+ * Initialize a dynamic table of 'len' lines. Columns are created at runtime.
+ * The variable argument list should finished by a COL_END entry.
+ * For each column to register, at least 2 arguments are required:
+ * 1. the column data type, and 2. the columns header string.
+ *
+ * Example to create a one column array of 100 lines:
+ *   log_vect_init(&d, 100, COL_INT16, "Col1Header", COL_END);
+ */
+void log_vect_init(datalog_t *d, uint16_t len, ...);
+
+/* Reset current logs data indexes to 0 */
+void log_vect_reset(datalog_t *d);
+
+/* Set value for a cell (ie. using column id) for the current line */
+void log_vect_setvalue(datalog_t *d, uint8_t idx, void * value);
+
+/* Move to next line for further data storage */
+void log_vect_storeline(datalog_t *d);
+
+/* Dump all table in CSV format */
+void log_vect_dumpall(datalog_t *d);
 
 #endif /* LOG_H */
