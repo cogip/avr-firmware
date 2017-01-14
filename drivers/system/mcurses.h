0e3e1eb0da4a294a1490de72e9722f201f6394eb
Yannick Gicquel <ygicquel@gmail.com>
2017-01-14 16:52:23 +0100

    mcurses: can be switched on/off in menuconfig
    This is intented to quickly switch on/off mcurses monitoring
    without changing already entry point in place.
    Here below are some flash usage statistics:
    
    1. Mcurses builtin:
    
    Program:   20030 bytes (14.4% Full)
    (.text + .data + .bootloader)
    
    Data:       2094 bytes (25.6% Full)
    (.data + .bss + .noinit)
    
    2. Mcurses stubbed:
    
    Program:   18542 bytes (13.3% Full)
    (.text + .data + .bootloader)
    
    Data:       1788 bytes (21.8% Full)
    (.data + .bss + .noinit)
    
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- drivers/system/mcurses.h
+++ drivers/system/mcurses.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+#ifdef CONFIG_MCURSES
+
 void mcurses_getyx(uint8_t *y, uint8_t *x);
 void mcurses_getmaxyx(uint8_t *y, uint8_t *x);
 
@@ -12,5 +14,19 @@ void mcurses_clear();
 void mcurses_init();
 
 void mcurses_monitor_printf(uint8_t idx, const char *format, ...);
+
+#else
+
+#define mcurses_getyx(y,x)
+#define mcurses_getmaxyx(y,x)
+
+#define mcurses_gotoyx(y,x)
+
+#define mcurses_clear()
+#define mcurses_init()
+
+#define mcurses_monitor_printf(i, f, ...);
+
+#endif /* CONFIG_MCURSES */
 
 #endif /* MCURSES_H */
