61cd1a0afa1c7d29f6735e17f16d4a86994e7b67
Yannick Gicquel <ygicquel@gmail.com>
2017-04-09 18:31:11 +0200

    controller: kp,ki & kd tuning for all pid
    using "n" and "b", user can select one of the available pid
    to tune its value afterward.
    
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- core/controller.c
+++ core/controller.c
@@ -464,7 +464,39 @@ void task_controller_update()
 }
 
 #if defined(CONFIG_CALIBRATION)
-static void controller_calibration_usage(void)
+static PID_t *controller_get_pid_from_idx(const uint8_t i)
+{
+	switch(i) {
+	case 0:
+		return &controller.linear_speed_pid;
+	case 1:
+		return &controller.angular_speed_pid;
+	case 2:
+		return &controller.linear_pose_pid;
+	case 3:
+		return &controller.angular_pose_pid;
+	default:
+		return NULL;
+	}
+}
+
+static const char *controller_get_pid_name_from_idx(const uint8_t i)
+{
+	switch(i) {
+	case 0:
+		return "linear_speed_pid";
+	case 1:
+		return "angular_speed_pid";
+	case 2:
+		return "linear_pose_pid";
+	case 3:
+		return "angular_pose_pid";
+	default:
+		return NULL;
+	}
+}
+
+static void controller_calibration_usage(const uint8_t pid_idx)
 {
 	cons_printf("\n>>> Entering controller calibration mode\n\n");
 
@@ -473,6 +505,17 @@ static void controller_calibration_usage(void)
 	cons_printf("\t'2' to launch speed control loop only (out: cal2.csv)\n");
 	cons_printf("\t       this will calibrate : Kp, Ki & (Kd) for speed PID\n");
 	cons_printf("\t'3' to launch angular control loop only (out: cal3.csv)\n");
+	cons_printf("\n");
+	cons_printf("\t'v' to dump all PID values\n");
+	cons_printf("\t'n' to select next PID to be tuned\n");
+	cons_printf("\t'b' to select prev PID to be tuned\n");
+	cons_printf("\n");
+	cons_printf("\%s:\t"
+	       "Kp = %+.2f\tKi = %+.2f\tKd = %+.2f\n",
+	       controller_get_pid_name_from_idx(pid_idx),
+	       controller_get_pid_from_idx(pid_idx)->kp,
+	       controller_get_pid_from_idx(pid_idx)->ki,
+	       controller_get_pid_from_idx(pid_idx)->kd);
 	cons_printf("\t'p' to tune Kp\n");
 	cons_printf("\t'i' to tune Ki\n");
 	cons_printf("\t'd' to tune Kd\n");
@@ -491,10 +534,12 @@ static void scanf_update_val (const char *var_name, double *var)
 
 void controller_enter_calibration()
 {
-	int c;
+	int c, i;
 	uint8_t quit = 0;
+	uint8_t pid_idx = 0;
+	PID_t *cur_pid = controller_get_pid_from_idx(pid_idx);
 
-	controller_calibration_usage();
+	controller_calibration_usage(pid_idx);
 
 	while (!quit) {
 
@@ -519,27 +564,40 @@ void controller_enter_calibration()
 			cons_printf("CAL3 launched\n");
 			break;
 		case 'p':
-			scanf_update_val("Kp", &controller.linear_speed_pid.kp);
+			scanf_update_val("Kp", &cur_pid->kp);
 			break;
 		case 'i':
-			scanf_update_val("Ki", &controller.linear_speed_pid.ki);
+			scanf_update_val("Ki", &cur_pid->ki);
 			break;
 		case 'd':
-			scanf_update_val("Kd", &controller.linear_speed_pid.kd);
+			scanf_update_val("Kd", &cur_pid->kd);
 			break;
-		case 'l':
-			cons_printf("controller.linear_speed_pid: "
-			       "Kp = %+.2f\tKi = %+.2f\tKd = %+.2f\n",
-			       controller.linear_speed_pid.kp,
-			       controller.linear_speed_pid.ki,
-			       controller.linear_speed_pid.kd);
-
-			scanf_update_val("Kp", &controller.linear_speed_pid.kp);
-			scanf_update_val("Ki", &controller.linear_speed_pid.ki);
-			scanf_update_val("Kd", &controller.linear_speed_pid.kd);
+		case 'n':
+			pid_idx += 1;
+			pid_idx %= 4;
+			cur_pid = controller_get_pid_from_idx(pid_idx);
+			controller_calibration_usage(pid_idx);
+			break;
+		case 'b':
+			if (!pid_idx)
+				pid_idx = 3;
+			else
+				pid_idx -= 1;
+			cur_pid = controller_get_pid_from_idx(pid_idx);
+			controller_calibration_usage(pid_idx);
+			break;
+		case 'v':
+			for (i = 0; i < 4; i++) {
+				cons_printf("\%s:\t"
+				       "Kp = %+.2f\tKi = %+.2f\tKd = %+.2f\n",
+				       controller_get_pid_name_from_idx(i),
+				       controller_get_pid_from_idx(i)->kp,
+				       controller_get_pid_from_idx(i)->ki,
+				       controller_get_pid_from_idx(i)->kd);
+			}
 			break;
 		case 'h':
-			controller_calibration_usage();
+			controller_calibration_usage(pid_idx);
 			break;
 		case 'q':
 			quit = 1;
