50f71dd9a77d9025c21a149423780eba9bce2555
Gilles DOFFE <g.doffe@gmail.com>
2017-05-17 19:56:13 +0200

    cortex: add obstacles creation function mach_fixed_obstacles_init()
    Signed-off-by: Gilles DOFFE <g.doffe@gmail.com>

---
+++ machines/cortex/obstacle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "avoidance.h"
+
+/* Init all known fixed obstacles on map */
+void mach_fixed_obstacles_init(void)
+{
+	/*polygon_t polygon;
+	uint8_t nb_vertices;
+
+	polygon.count = 0;
+	nb_vertices = 4;
+	if (nb_vertices < POLY_MAX_POINTS)
+	{
+		polygon.points[polygon.count++] = (pose_t){.x = , .y = };
+		polygon.points[polygon.count++] = (pose_t){.x = , .y = };
+		polygon.points[polygon.count++] = (pose_t){.x = , .y = };
+		polygon.points[polygon.count++] = (pose_t){.x = , .y = };
+		add_polygon(&polygon);
+	}
+	*/
+}
+
