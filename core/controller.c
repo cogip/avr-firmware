73db41bff14bda07a2f9e1eff0f9d73caf6ffe16
Yannick Gicquel <ygicquel@gmail.com>
2017-05-20 13:25:02 +0200

    controller: remove extra conversions
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- core/controller.c
+++ core/controller.c
@@ -152,8 +152,7 @@ polar_t controller_update(controller_t *ctrl,
 		/* final orientation error */
 		if (!ctrl->pose_intermediate)
 		{
-			position_error.angle = limit_angle_rad(DEG2RAD(pose_order.O) - DEG2RAD(pose_current.O));
-			position_error.angle = RAD2DEG(position_error.angle);
+			position_error.angle = limit_angle_deg(pose_order.O - pose_current.O);
 		}
 
 		position_error.distance = 0;
