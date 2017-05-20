229b20d7b5bab157d942e26f2b4f4a56c1fceacb
Yannick Gicquel <ygicquel@gmail.com>
2017-05-20 11:58:51 +0200

    cortex: homologation path
    from yellow camp
    
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- machines/cortex/path.c
+++ machines/cortex/path.c
@@ -4,6 +4,36 @@
 
 #include "utils.h"
 
+// TODO: do not use Rear gear.
+path_pose_t path_homologation_yellow[] = {
+	[0] = {
+		.pos = {
+			.x = 1400,
+			.y = 500,
+			.O = 135,
+		},
+		.act = NULL,
+	},
+	[1] = {
+		.pos = {
+			.x = 1000,
+			.y = 900,
+			.O = -90,
+		},
+		.act = NULL,
+	},
+	[2] = {
+		.pos = {
+			.x = 950,
+			.y = 400,
+			.O = -90,
+		},
+		.act = NULL,
+	},
+};
+
+uint8_t path_homologation_yellow_nb = 3;
+
 path_pose_t path_game_yellow[] = {
 	[0] = {
 		.pos = {
