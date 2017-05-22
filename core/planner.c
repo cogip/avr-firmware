da538486656775f6b3c6873b90b442a26fe8ac93
Yannick Gicquel <ygicquel@gmail.com>
2017-05-22 20:29:30 +0200

    planner: calibration of next point to reach using console
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- core/planner.c
+++ core/planner.c
@@ -234,9 +234,19 @@ static void planner_calibration_usage()
 	cons_printf("\t'a' to launch current action at current pose\n");
 	cons_printf("\n");
 	cons_printf("\t'v' to dump all pose in path\n");
+	cons_printf("\t'x' to tune next pose x\n");
+	cons_printf("\t'y' to tune next pose y\n");
+	cons_printf("\t'O' to tune next pose O\n");
 	cons_printf("\t'h' to display this help\n");
 	cons_printf("\t'q' to quit\n");
 	cons_printf("\n");
+}
+
+static void scanf_update_val (const char *var_name, double *var)
+{
+	cons_printf("%s = %+.2f\tenter new value: ", var_name , *var);
+	cons_scanf("%lf", var);
+	cons_printf("new %s = %+.2f\n", var_name, *var);
 }
 
 void planner_enter_calibration()
@@ -269,6 +279,18 @@ void planner_enter_calibration()
 		case 'a':
 			if (path->poses[path->current_pose_idx].act)
 				path->poses[path->current_pose_idx].act();
+			break;
+		case 'x':
+			if (path->current_pose_idx+1 < path->nb_pose)
+				scanf_update_val("x", &path->poses[path->current_pose_idx+1].pos.x);
+			break;
+		case 'y':
+			if (path->current_pose_idx+1 < path->nb_pose)
+				scanf_update_val("y", &path->poses[path->current_pose_idx+1].pos.y);
+			break;
+		case 'O':
+			if (path->current_pose_idx+1 < path->nb_pose)
+				scanf_update_val("O", &path->poses[path->current_pose_idx+1].pos.O);
 			break;
 		case 'v':
 			cons_printf("nb_pose = %d\n", path->nb_pose);
