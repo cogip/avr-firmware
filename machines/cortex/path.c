af2e0a2de04be0e8f1491ebf03a0c84a17bb5b65
Yannick Gicquel <ygicquel@gmail.com>
2017-05-22 20:41:44 +0200

    cortex: path in a compact syntax
    to ease edition during the rush
    
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- machines/cortex/path.c
+++ machines/cortex/path.c
@@ -6,37 +6,10 @@
 
 // TODO: do not use Rear gear.
 static path_pose_t poses_homologation_yellow[] = {
-	{ /* POSE_INITIAL */
-		.pos = {
-			.x = 873,
-			.y = 163,
-			.O = 45,
-		},
-	},
-	{
-		.pos = {
-			.x = 1400,
-			.y = 500,
-			.O = 135,
-		},
-		.act = NULL,
-	},
-	{
-		.pos = {
-			.x = 1000,
-			.y = 900,
-			.O = -90,
-		},
-		.act = NULL,
-	},
-	{
-		.pos = {
-			.x = 950,
-			.y = 400,
-			.O = -90,
-		},
-		.act = NULL,
-	},
+	{ .pos = {.x =  873, .y =  163, .O =   45, }, .act = NULL, }, /* POSE_INITIAL */
+	{ .pos = {.x = 1400, .y =  500, .O =  135, }, .act = NULL, },
+	{ .pos = {.x = 1000, .y =  900, .O =  -90, }, .act = NULL, },
+	{ .pos = {.x =  950, .y =  400, .O =  -90, }, .act = NULL, },
 };
 
 path_t path_homologation_yellow = {
