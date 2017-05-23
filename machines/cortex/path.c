59b71e60f5d25011fc3db17029fccb52afe785d6
Yannick Gicquel <ygicquel@gmail.com>
2017-05-23 23:43:12 +0200

    cortex: path compact syntax
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- machines/cortex/path.c
+++ machines/cortex/path.c
@@ -19,57 +19,19 @@ path_t path_homologation_yellow = {
 	.poses = poses_homologation_yellow,
 };
 
-#if 0
-path_pose_t path_game_yellow[] = {
-	[0] = {
-		.pos = {
-			.x = 1117,
-			.y = 260,
-			.O = 0,
-		},
-		.act = NULL,
-	},
-	[1] = {
-		.pos = {
-			.x = 1187,
-			.y = 260,
-			.O = 180,
-		},
-		.act = NULL,
-	},
-	[2] = {
-		.pos = {
-			.x = 1187,
-			.y = 260,
-			.O = 180,
-		},
-		.act = NULL,
-	},
-	[3] = {
-		.pos = {
-			.x = 1117,
-			.y = 260,
-			.O = 179,
-		},
-		.act = NULL,
-	},
-	[4] = {
-		.pos = {
-			.x = 1267,
-			.y = 1150,
-			.O = 90,
-		},
-		.act = NULL,
-	},
-	[5] = {
-		.pos = {
-			.x = 1267,
-			.y = 1120,
-			.O = 90,
-		},
-		.act = NULL,
-	},
+static path_pose_t path_game_yellow[] = {
+	{ .pos = {.x =  873, .y =  163, .O =   45, }, .act = NULL, }, /* POSE_INITIAL */
+	{ .pos = {.x = 1117, .y =  260, .O =    0, }, .act = NULL, },
+	{ .pos = {.x = 1187, .y =  260, .O =  180, }, .act = NULL, },
+	{ .pos = {.x = 1187, .y =  260,	.O =  180, }, .act = NULL, },
+	{ .pos = {.x = 1117, .y =  260, .O =  179, }, .act = NULL, },
+	{ .pos = {.x = 1267, .y = 1150,	.O =   90, }, .act = NULL, },
+	{ .pos = {.x = 1267, .y = 1120, .O =   90, }, .act = NULL, },
 };
 
-uint8_t path_game_yellow_nb = 6;
-#endif
+path_t path_yellow = {
+	/* static cfg */
+	.play_in_loop = FALSE,
+	.nb_pose = 7,
+	.poses = path_game_yellow,
+};
