9ec6aff285061b4b7508214188a3be458b6b142f
Yannick Gicquel <ygicquel@gmail.com>
2017-05-24 19:55:44 +0200

    cortex: remove little arm control
    Signed-off-by: Yannick Gicquel <ygicquel@gmail.com>

--- machines/cortex/action.c
+++ machines/cortex/action.c
@@ -20,9 +20,6 @@ void act_catch_module_front_right(void)
 {
 	uint8_t pause_ms = 250 / 20;
 
-	/* wedge right open */
-	sd21_control_servo(&sd21, SERVO_ID_WEDGE_R, SD21_SERVO_OPEN);
-
 	/* ventouse front right open */
 	sd21_control_servo(&sd21, SERVO_ID_VENT_FR, SD21_SERVO_OPEN);
 
@@ -49,9 +46,6 @@ void act_catch_module_rear_right(void)
 	while (pause_ms--)
 		kos_yield();
 
-	/* wedge right close */
-	sd21_control_servo(&sd21, SERVO_ID_WEDGE_R, SD21_SERVO_CLOSE);
-
 	/* ventouse rear right close */
 	sd21_control_servo(&sd21, SERVO_ID_VENT_RR, SD21_SERVO_CLOSE);
 }
@@ -62,9 +56,6 @@ void act_catch_module_front_left(void)
 
 	/* ventouse front left open */
 	sd21_control_servo(&sd21, SERVO_ID_VENT_FL, SD21_SERVO_OPEN);
-
-	/* wedge left open */
-	sd21_control_servo(&sd21, SERVO_ID_WEDGE_L, SD21_SERVO_OPEN);
 
 	/* pump front left on */
 	gpio_set_output(&PORTB, GPIO_ID_PUMP_FL, 1);
@@ -88,9 +79,6 @@ void act_catch_module_rear_left(void)
 
 	while (pause_ms--)
 		kos_yield();
-
-	/* wedge left close */
-	sd21_control_servo(&sd21, SERVO_ID_WEDGE_L, SD21_SERVO_CLOSE);
 
 	/* ventouse rear left close */
 	sd21_control_servo(&sd21, SERVO_ID_VENT_RL, SD21_SERVO_CLOSE);
